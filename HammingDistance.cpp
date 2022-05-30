994376657bc8ec06456d492b877033cbf9bb0293
Dominik Edgar Szpilski <kontaktedgarsz@gmail.com>
2022-05-30 21:12:02 +0200

    basic implementation of arugments handling

--- HammingDistance.cpp
+++ HammingDistance.cpp
@@ -9,7 +9,21 @@ struct berResults
     clock_t t2;
 };
 
-int main()
+int main(int argc, char* argv[])
 {
-    std::cout << "Hello World!\n";
+    std::string arg1;
+    std::string arg2;
+    openLog("log.txt");
+    if (argc != 3) {
+        saveLog("Critical: Arguments not specified.");
+        saveLog("Re-run program with proper arguments ie: ./HammingDistance.exe <test_file1> <test_file2>");
+        return 1;
+    }
+    else {
+        arg1 = argv[1];
+        arg2 = argv[2];
+        std::string logMessage = "Provided arguments: arg1<" + arg1 + "> arg2<" + arg2 + ">";
+        saveLog(logMessage);
+    }
+    return 0;
 }
