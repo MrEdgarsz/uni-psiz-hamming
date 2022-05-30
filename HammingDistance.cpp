cf35857da5266d791498081cb938fe337d1503d9
Dominik Edgar Szpilski <kontaktedgarsz@gmail.com>
2022-05-30 21:24:50 +0200

    implemented function to generate files with random values

--- HammingDistance.cpp
+++ HammingDistance.cpp
@@ -10,6 +10,8 @@ struct berResults
 };
 
 void createFile(const std::string name, const int count, const char value);
+void createFileWithRandomValues(const std::string name, const int count, const char value);
+char* getRandomBytes(const int count);
 int main(int argc, char* argv[])
 {
     std::string arg1;
@@ -38,4 +40,33 @@ void createFile(const std::string name, const int count, const char value)
         f.write((char*)&value, 1);
     }
     f.close();
+}
+
+void createFileWithRandomValues(const std::string name, const int count, const char value)
+{
+    std::fstream f;
+    char* randomValue = new char[5];
+    strcpy(randomValue, getRandomBytes(count));
+
+    f.open(name.c_str(), std::ios::binary | std::ios::out);
+    for (int i = 0; i < count; i++)
+    {
+        if (i % 10 == 0) {
+            f.write((char*)&randomValue, 1);
+        }
+        else {
+
+            f.write((char*)&value, 1);
+        }
+    }
+    f.close();
+}
+char* getRandomBytes(const int count) {
+    srand(time(NULL));
+    int random = rand() % 5 + 2;
+    std::string suffix = std::to_string((count / random) + random);
+    std::string stringValue = "0x" + suffix;
+    char* charValue = new char[5];
+    strcpy(charValue, stringValue.c_str());
+    return charValue;
 }
